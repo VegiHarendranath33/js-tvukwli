bdcec106e43039b6baf0c0e8175ff36d319acedb
VegiHarendranath33 <108779050+VegiHarendranath33@users.noreply.github.com>
2022-10-12 15:28:06 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-tvukwli
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-n4xxkt)
---
+++ index.js
@@ -0,0 +1,33 @@
+// var marks = 75;
+// if (marks >80 )
+// {
+//   console.log('GRADE A');
+// }
+// else 
+// {
+//     console.log('GRADE B');
+// }
+
+// var marks = 40;
+//  if (marks >80 )
+//  {   console.log('GRADE A');
+//  }
+//   else if (marks > 55)
+//   {
+//     console.log('GRADE B');
+//   }
+//  else 
+//  {
+//    console.log('GRADE C');
+// }
+// for (var i =0; i<5; i++)
+// {
+//     console.log('hari');
+// }
+
+ var output = 0;
+ for (var i =1; i<=10; i++)
+ {
+   output = output + i;
+}
+console.log(output);
