4f36e8bf2cfe03e15193bcf9b887d4030a2b4eed
VegiHarendranath33 <108779050+VegiHarendranath33@users.noreply.github.com>
2022-10-12 15:36:46 +0530

    1 file modified

--- index.js
+++ index.js
@@ -32,9 +32,142 @@
 // }
 // console.log(output);
 
-var output = 1;
-while (output <=10)
-{
-  output++;
-  console.log(output);
-}
+// var output = 1;
+// while (output <=10)
+// {
+//   output++;
+//   console.log(output);
+// }
+
+// # what is reactjs
+
+//  reactjs is A JAVASCRIPT library  used to develop user  Interface component 
+//  Basic requirements :
+
+//  1.HTML
+//  2.c                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+//  3.Javascript (Es6)
+
+//  Fundamental concepts:
+//  Variable Declaration
+//  Data Types
+//  Arrays and Objects 
+//  Functions
+//  Conditional Statements
+//  Looping Concept
+
+
+//  Variable Declaration:
+
+
+//  playcode.io/new
+
+//  after  es5 let and const have been introduced 
+
+//  var was used to declare a variable
+//  variable declaration //
+
+//  var  name = 'hari';
+//  console.log(name);
+
+//  4 data types
+//  //number string boolean undefined
+
+//  ==============================
+//  var data = 1;
+//  // var data = 1.5;
+
+//  // var data = "hari";
+//  console.log(typeof data);  ans: number.
+//  ======================
+
+// when u are trying to store any characters enclosed in single quotes or double quotes are called as string.
+
+// boolean is used to store only 2 types of values.
+// one is true and another one is false.
+
+// true: satisfying Condition
+// false: unsatisfying Condition
+// ====================================
+// var data = true;  // t should not be capital letters if u give capital letter it says true is not defined
+// console.log(type of data)
+// ans : boolean
+// ======
+// var data;  we are defining a variable and we are not assigning any value then we get 
+// the console.log(typeof data);  as undefined.it is telling as object.
+// ====
+
+// variables 
+// data types
+
+// Arrays and objects
+// ===================
+// upto know we stored in one variable only one value
+// but in arrays we can store in one variable multiple values:
+
+// ===
+
+// var data = [1,2,3,4,5 ];
+// console.log(data);
+// Every thing in array is storing in  oth position 
+// 1 is oth index
+// based on index it occupies its position.
+// ===================================
+// console.log(data[0]);
+
+// ===
+// object is nothing but storing data in terms of name and value 
+// or key and value.
+
+// name - value
+// key  - value 
+
+// name
+// john  age=30
+
+// var data  = {
+//     name : 'HarendranathVegi';
+//     age : 44;
+//     console.log(data.age);
+// }
+
+// we are accessing  name property in  data object.
+
+// similiarly we are access age property in data object.
+
+// ================================
+
+// Functions: function is nothing but used to perform a specific task in a block of code is called function
+// ---
+// func user() {
+
+//     //block of code  we can write the action inside the function
+// }
+
+// user();
+
+// func smh() {
+//     console.log('smh members are sudarshan hari anjan')
+// }
+
+// smh();
+
+// ====
+// function user willbe executed whenever we are calling a block of code 
+
+
+// func user() {
+//     console.log('Hi Hari');
+// }
+// user();
+// =================
+
+// func hari(sal)
+// {
+//     return sal + 5;
+// }
+
+// var output = hari(2100000);
+// console.log(output);
+
+// ====
